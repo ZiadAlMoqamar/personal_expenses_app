9bce7d8871489d4f3307c78a4d4c689fad3ca437
ZiadAlMoqamar <ziad.almoqamar@gmail.com>
2021-09-01 20:26:24 +0200

    adding date format package

--- lib/components/myListTile.dart
+++ lib/components/myListTile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:personal_expenses_app/classes/transaction.dart';
+import 'package:intl/intl.dart';
 
 class myListTile extends StatelessWidget {
   final Transaction transaction;
@@ -7,48 +8,42 @@ class myListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(10),
-              topRight: Radius.circular(10),
-              bottomLeft: Radius.circular(10),
-              bottomRight: Radius.circular(10)),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 5,
-              blurRadius: 7,
-              offset: Offset(0, 3), // changes position of shadow
-            ),
-          ],
+    return Container(
+      margin: EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      child: ListTile(
+        leading: CircleAvatar(
+          radius: 30,
+          backgroundColor: Theme.of(context).primaryColor,
+          child: Text("\$${transaction.cost}",
+              style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold)),
         ),
-        child: ListTile(
-          leading: CircleAvatar(
-            radius: 30,
-            backgroundColor: Theme.of(context).primaryColor,
-            child: Text("\$${transaction.cost}",
-                style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold)),
-          ),
-          title: Text(
-            transaction.title,
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-          subtitle: Text(transaction.time.toString()),
-          trailing: IconButton(
-            icon: Icon(
-              Icons.delete,
-              color: Colors.red,
-            ),
-            onPressed: () => print('delete'),
-          ),
-          tileColor: Colors.white,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        title: Text(
+          transaction.title,
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
         ),
+        subtitle: Text(DateFormat().format(transaction.time)),
+        trailing: IconButton(
+          icon: Icon(
+            Icons.delete,
+            color: Colors.red,
+          ),
+          onPressed: () => print('delete'),
+        ),
+        tileColor: Colors.white,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       ),
     );
   }
