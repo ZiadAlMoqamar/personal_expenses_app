f428bb5e48ce75fcf394bee03472b93301059669
ZiadAlMoqamar <ziad.almoqamar@gmail.com>
2021-09-25 12:47:53 +0200

    making user transactions list responsive

--- lib/components/userTransactions.dart
+++ lib/components/userTransactions.dart
@@ -15,7 +15,8 @@ class _UserTransactionsListState extends State<UserTransactionsList> {
   @override
   Widget build(BuildContext context) {
     return widget.transactions.isEmpty
-        ? Column(
+        ? LayoutBuilder(builder: (ctx,constraints){
+          return Column(
             children: [
               Text(
                 'No transactions added yet!',
@@ -23,14 +24,15 @@ class _UserTransactionsListState extends State<UserTransactionsList> {
               ),
               SizedBox(height: 10,),
               Container(
-                height: MediaQuery.of(context).size.height *0.4,
+                height: constraints.maxHeight *0.6,
                 child: Image.asset(
                   'assets/images/waiting.png',
                   fit: BoxFit.cover,
                 ),
               ),
             ],
-          )
+          );
+        })
         : ListView.builder(
             itemBuilder: (ctx, index) {
               return MyListTile(transaction: widget.transactions[index], deleteTx: widget.deleteTx,);
