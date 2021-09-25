5f0f42a0e083feee9b542fa2cb94b6fd4e510c49
ZiadAlMoqamar <ziad.almoqamar@gmail.com>
2021-09-25 09:18:48 +0200

    making charts responsive

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 class Chart extends StatelessWidget {
@@ -16,50 +14,69 @@ class Chart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 20,
-          child: FittedBox(
-            child: Text('\$${totalSpending.toStringAsFixed(0)}',style: Theme.of(context).textTheme.subtitle1),
+    return LayoutBuilder(builder: (ctx, constraints) {
+      return Column(
+        children: [
+          Container(
+            height: constraints.maxHeight * 0.14,
+            child: FittedBox(
+              child: Text('\$${totalSpending.toStringAsFixed(0)}',
+                  style: Theme.of(context).textTheme.subtitle1),
+            ),
           ),
-        ),
-        SizedBox(
-          height: 4,
-        ),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey, width: 1),
-                  color: Color.fromRGBO(220, 220, 220, 1),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              FractionallySizedBox(
-                heightFactor: spendingToWeekRatio,
-                child: Container(
+          SizedBox(
+            height: constraints.maxHeight * 0.04,
+          ),
+          Container(
+            height: constraints.maxHeight * 0.5,
+            width: 10,
+            child: Stack(
+              children: [
+                Container(
                   decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
+                    border: Border.all(color: Colors.grey, width: 1),
+                    color: Color.fromRGBO(220, 220, 220, 1),
                     borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-              )
-            ],
+                FractionallySizedBox(
+                  heightFactor: spendingToWeekRatio,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-        SizedBox(
-          height: 4,
-        ),
-        Text(title, style: Theme.of(context).textTheme.bodyText2,),
-        SizedBox(
-          height: 4,
-        ),
-        Text(dayDate, style: Theme.of(context).textTheme.caption,),
-      ],
-    );
+          SizedBox(
+            height: constraints.maxHeight * 0.04,
+          ),
+          Container(
+            height: constraints.maxHeight * 0.14,
+            child: FittedBox(
+              child: Text(
+                title,
+                style: Theme.of(context).textTheme.bodyText2,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: constraints.maxHeight * 0.04,
+          ),
+          Container(
+            height: constraints.maxHeight * 0.10,
+            child: FittedBox(
+              child: Text(
+                dayDate,
+                style: Theme.of(context).textTheme.caption,
+              ),
+            ),
+          ),
+        ],
+      );
+    });
   }
 }
